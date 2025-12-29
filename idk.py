774022d125930f665f6971a2ba1c67f0b2995457
Hmm Stefan <hmmfanu@gmail.com>
2025-12-29 09:10:56 +0200

    remove_useless_files

--- idk.py
+++
@@ -1,4 +0,0 @@
-import torch
-print(torch.cuda.is_available())        # Should be True
-print(torch.cuda.device_count())        # Should be 1 for RTX 3050
-print(torch.cuda.get_device_name(0))    # RTX 3050
